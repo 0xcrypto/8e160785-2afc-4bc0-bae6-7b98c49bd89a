19c2f1ec5b2fc20382b3ca82d1123a814828f4f9
Ankit Sharma <ankitit2015@gmail.com>
2018-11-03 11:42:06 +0530

    Add project files.

---
+++ Parking.Common/MPSSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Parking.Common
+{
+    public class MPSSettings
+    {
+        public string MPSDeviceID { get; set; }
+        public string UserID { get; set; }
+        public string UserPassword { get; set; }
+    }
+}
